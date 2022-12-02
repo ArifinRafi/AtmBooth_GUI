e983c827d0cf1b71c0954569ace6d468a932c452
arifin rafi <ahmedrafi364@gmail.com>
2022-12-02 19:28:23 +0600

    first commit

---
+++ js/deposit.js
@@ -0,0 +1,18 @@
+document.getElementById('deposit-btn').addEventListener('click', function() {
+   
+    const inputDepositAmaount = get_input_element('deposit-field');
+    const prevDepositAmaount = get_text_element('deposit-print');
+    const newDepositAmaount = inputDepositAmaount + prevDepositAmaount;
+    set_newValue('deposit-print', newDepositAmaount);
+
+    const prevBalance = get_text_element('balance-print');
+    const newBalance = prevBalance + inputDepositAmaount;
+    set_newValue('balance-print', newBalance);
+
+
+    
+
+
+})
+
+
---
+++ js/utilities.js
@@ -0,0 +1,23 @@
+function get_input_element(idOftheElement) {
+   const depositField = document.getElementById(idOftheElement);
+   const stringvalue = depositField.value;
+   depositAmount = parseFloat(stringvalue);
+   depositField.value = '';
+   return depositAmount;
+
+  
+}
+
+
+function get_text_element(idOftheElement) {
+    const prev_value = document.getElementById(idOftheElement);
+    const prev_string_field = prev_value.innerText;
+    const prev_int_field = parseFloat(prev_string_field);
+    return prev_int_field;
+
+}
+
+function set_newValue(idOftheElement, newVal) {
+    const prev_val = document.getElementById(idOftheElement);
+    prev_val.innerText = newVal;
+}
